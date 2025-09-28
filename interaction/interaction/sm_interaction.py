ed325d03a4e56f8500c0d0860019d992884a74ad
fernanda <fecorrea.barbosa@gmail.com>
2025-09-28 08:19:02 -0300

    feat: Implement main State Machine for gesture navigation.

---
+++ interaction/interaction/sm_interaction.py
@@ -0,0 +1,63 @@
+import rclpy
+import time
+
+from yasmin import StateMachine, State
+from yasmin_ros.basic_outcomes import SUCCEED, ABORT, TIMEOUT
+from yasmin_ros.yasmin_node import YasminNode
+
+from interaction.states.basic_states import (
+    Initialize, 
+    Takeoff,
+    FindHuman,
+    FollowHuman,
+    ReturnToLaunch
+)
+
+class CBRPhase3StateMachine(StateMachine):
+
+    def __init__(self):
+        super().__init__(outcomes=[SUCCEED, ABORT])
+
+        self.add_state(
+            "INITIALIZE", Initialize(), transitions={SUCCEED: "TAKEOFF", ABORT: "END"}
+        )
+
+        self.add_state(
+            "TAKEOFF", Takeoff(), transitions={SUCCEED: "FIND_HUMAN", ABORT: "END"}
+        )
+
+        self.add_state(
+            "FIND_HUMAN", FindHuman, transitions={SUCCEED: "FOLLOW_HUMAN", ABORT: "RETURN_TO_LAUNCH"}
+        )
+
+        self.add_state(
+            "FOLLOW_HUMAN", FollowHuman, transitions={SUCCEED: "RETURN_TO_LAUNCH", ABORT: "RETURN_TO_LAUNCH"}
+        )
+
+        self.add_state(
+            "RETURN_TO_LAUNCH", ReturnToLaunch, transitions={SUCCEED: "END", ABORT:"END"}
+        )
+
+        self.add_state("END", End(), transitions={SUCCEED: SUCCEED})
+
+
+
+def main() -> None:
+    print("CBR 2025 - PHASE 3: HUMAN INTERACTION")
+
+    rclpy.init()
+
+    try:
+        phase3_sm = CBRPhase3StateMachine()
+
+        print(phase3_sm())
+
+    except Exception as e:
+        print(f"Mission failed with exception: {e}")
+
+    finally:
+        rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
