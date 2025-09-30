e3e159ab6911393f094b3ddbd38351ddf52b95ea
lucianosantos23 <lucianoaraujodossantosfilho@gmail.com>
2025-09-30 14:36:02 -0300

    add: check_pkg logic

--- delivery/delivery/states/pickup/pickup_states/check_pkg.py
+++ delivery/delivery/states/pickup/pickup_states/check_pkg.py
@@ -1,15 +1,36 @@
+import time
 import yasmin
 from yasmin import State, Blackboard
-from yasmin_ros.basic_outcomes import SUCCEED, ABORT
-
+from yasmin_ros.basic_outcomes import SUCCEED
+from delivery.utils.yolo_detection import YOLODetector
+from mirela_sdk.image_processing.camera.image_handler import ImageHandler
 
 class CheckPkg(State):
     """
-    Checar pela yolo se o pacote ainda esta na base
+    Checar pela YOLO se o pacote ainda está na base.
     """
     def __init__(self):
-        super().__init__(outcomes=[SUCCEED, ABORT, "pkg_detected"])
+        super().__init__(outcomes=[SUCCEED, "pkg_detected"])
+        self.detector = YOLODetector()
 
     def execute(self, blackboard: Blackboard):
-        yasmin.YASMIN_LOG_INFO("CheckPkg (stub) executado.")
+        image_handler: ImageHandler = blackboard.get("image_handler")
+
+        for _ in range(3):
+            time.sleep(1)
+            image = image_handler.take_photo()
+
+            detection = self.detector.detect(desired_class="package", image=image, save_image=False)
+
+            if detection:
+                yasmin.YASMIN_LOG_INFO("CheckPkg: pacote detectado!!!")
+                return "pkg_detected"
+
+        yasmin.YASMIN_LOG_INFO("CheckPkg: Nenhum pacote detectado após 3 tentativas.")
         return SUCCEED
+
+
+
+
+    
+        
